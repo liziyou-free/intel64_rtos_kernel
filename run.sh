06d98f6b2733d9d98a49d6e102eea97b0712fd1a
freedomli <1165147055@qq.com>
2023-11-09 18:03:49 +0800

    temp1

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cp ./Debug/x86_64_prj.elf  root
 grub-mkrescue -o multiboot.iso root
 
-qemu-system-x86_64 -cdrom multiboot.iso -boot d  
+qemu-system-x86_64 -cdrom multiboot.iso -cpu host -enable-kvm -boot d  -S -s
