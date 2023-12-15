40a08a85e166e4ce44c9e2786def319266a3ff86
FreedomLi <1165147055@qq.com>
2023-12-15 05:03:19 -0500

    temp28.

--- x64_cpu_drivers/x64_pcie.c
+++ x64_cpu_drivers/x64_pcie.c
@@ -22,10 +22,19 @@
 #include "../startup/x64_common.h"
 
 
+
+#define PRINT_PCI_DEV_SYMBOL() \
+    printf("****************************************************\r\n"); \
+    printf("*            ----- PCI DEVICE INFO -------         *\r\n"); \
+    printf("****************************************************\r\n");
+
+
+
+
 #define __ENABLE_CONFIG_SPACE_MAP__    (1ul << 31)
 
 
-void pci_config_space_write(uint32_t bdf, uint32_t reg, uint32_t data)
+void pci_atomic_write(uint32_t bdf, uint32_t reg, uint32_t data)
 {
     uint32_t addr_port;
 
@@ -36,7 +45,7 @@ void pci_config_space_write(uint32_t bdf, uint32_t reg, uint32_t data)
 }
 
 
-uint32_t pci_config_space_read(uint32_t bdf, uint32_t reg)
+uint32_t pci_atomic_read(uint32_t bdf, uint32_t reg)
 {
     uint32_t addr_port;
     uint32_t data;
@@ -51,18 +60,52 @@ uint32_t pci_config_space_read(uint32_t bdf, uint32_t reg)
 
 #define BDF(b, d, f)    ((b << 8) | (d << 3) | f)
 
-void pcie_device_enum()
+
+void pcie_read_config_header(uint32_t bdf, cfg_header_type0_t *obj)
 {
-    uint32_t info[16] = {0};
+    uint32_t *info;
+
+    info = (uint32_t *)obj;
 
     for (int j = 0; j < 16; j++) {
-        info[j] = pci_config_space_read(0, j);
-    }
-    for (int j = 0; j < 16; j++) {
-        info[j] = pci_config_space_read(BDF(0, 3, 0), j);
+        info[j] = pci_atomic_read(0, j);
     }
     return;
 }
+
+
+void dump_pcie_dev_info(uint32_t bdf)
+{
+    #define PRINT_LINE(str, __VA_ARGS__) \
+                printf(str "\r\n", __VA_ARGS__)
+
+    cfg_header_type0_t obj;
+
+    PRINT_PCI_DEV_SYMBOL();
+
+    pcie_read_config_header(bdf, &obj);
+    PRINT_LINE("Vendor-ID: %#x", obj.vendor_id);
+    PRINT_LINE("Device-ID: %#x", obj.device_id);
+    PRINT_LINE("BA0: %#x", obj.base_addr[0]);
+    PRINT_LINE("BA0: %#x", obj.base_addr[1]);
+    PRINT_LINE("BA0: %#x", obj.base_addr[2]);
+    PRINT_LINE("BA0: %#x", obj.base_addr[3]);
+    PRINT_LINE("BA0: %#x", obj.base_addr[4]);
+    PRINT_LINE("BA0: %#x", obj.base_addr[5]);
+    PRINT_LINE("Class-Code: %#x", obj.class_code);
+    PRINT_LINE("Header-Type: %#x", obj.header_type);
+    PRINT_LINE("Interrupt-Line: %#x", obj.interrupt_line);
+    PRINT_LINE("Interrupt-Pin: %#x", obj.interrupt_pin);
+    return;
+}
+
+
+void pcie_device_enum()
+{
+    dump_pcie_dev_info(BDF(0, 9, 0));
+    return;
+}
+
 
 
 
--- x64_cpu_drivers/x64_pcie.h
+++ x64_cpu_drivers/x64_pcie.h
@@ -20,19 +20,47 @@
 #ifndef X64_PCIE_H_
 #define X64_PCIE_H_
 
-//struct  config_header_type0{
-//
-//};
-//
-//
-//struct  config_header_type1{
-//
-//};
-//
-//
-//struct {
-//
-//}pci_compatible_config_space_t;
+
+#pragma pack(push)
+#pragma pack(1)
+
+typedef struct {
+    uint16_t vendor_id;
+    uint16_t device_id;
+    uint16_t command;
+    uint16_t status;
+    uint16_t revision_id;
+    uint16_t class_code;
+    uint8_t  cache_line_size;
+    uint8_t  latency_timer;
+    uint8_t  header_type;
+    uint8_t  bist;
+    uint32_t base_addr[6];
+    uint32_t cardbus_cis_pointer;
+    uint16_t subsystem_vendor_id;
+    uint16_t subsystem_id;
+    uint32_t expansion_rom_base_addr;
+    uint8_t  capability_pointer;
+    uint8_t  reserved[7];
+    uint8_t  interrupt_line;
+    uint8_t  interrupt_pin;
+    uint8_t  min_gnt;
+    uint8_t  max_lat;
+} cfg_header_type0_t;
+
+
+struct  config_header_type1{
+
+};
+
+
+struct pci_compatible_config_space_t{
+
+};
+
+
+#pragma pack(pop)
+
 
 
 /**
