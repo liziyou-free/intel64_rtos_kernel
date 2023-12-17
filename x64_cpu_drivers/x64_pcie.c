58723c176f1ac6549e7fb4127c8f22413021195e
FreedomLi <1165147055@qq.com>
2023-12-18 01:41:54 +0800

    add pcie enum.

--- x64_cpu_drivers/x64_pcie.c
+++ x64_cpu_drivers/x64_pcie.c
@@ -18,20 +18,29 @@
 *******************************************************************************/
 
 #include <stdint.h>
+#include <stdio.h>
 #include "./x64_pcie.h"
 #include "../startup/x64_common.h"
 
-
-
 #define PRINT_PCI_DEV_SYMBOL() \
+    printf("\r\n"); \
     printf("****************************************************\r\n"); \
     printf("*            ----- PCI DEVICE INFO -------         *\r\n"); \
     printf("****************************************************\r\n");
 
-
+#define PRINT_PCI_BRIDGE_SYMBOL() \
+    printf("\r\n"); \
+    printf("****************************************************\r\n"); \
+    printf("*            ----- PCI BRIDGE INFO -------         *\r\n"); \
+    printf("****************************************************\r\n");
 
 
 #define __ENABLE_CONFIG_SPACE_MAP__    (1ul << 31)
+
+#define BDF(b, d, f)    ((b << 8) | (d << 3) | f)
+
+
+char* classcode_to_text(class_code_t *classcode);
 
 
 void pci_atomic_write(uint32_t bdf, uint32_t reg, uint32_t data)
@@ -58,17 +67,15 @@ uint32_t pci_atomic_read(uint32_t bdf, uint32_t reg)
 }
 
 
-#define BDF(b, d, f)    ((b << 8) | (d << 3) | f)
 
-
-void pcie_read_config_header(uint32_t bdf, cfg_header_type0_t *obj)
+void pcie_read_config_header(uint32_t bdf, void *obj)
 {
     uint32_t *info;
 
     info = (uint32_t *)obj;
 
     for (int j = 0; j < 16; j++) {
-        info[j] = pci_atomic_read(0, j);
+        info[j] = pci_atomic_read(bdf, j);
     }
     return;
 }
@@ -76,38 +83,163 @@ void pcie_read_config_header(uint32_t bdf, cfg_header_type0_t *obj)
 
 void dump_pcie_dev_info(uint32_t bdf)
 {
-    #define PRINT_LINE(str, __VA_ARGS__) \
+    #define PRINT_LINE(str, ...) \
                 printf(str "\r\n", __VA_ARGS__)
 
-    cfg_header_type0_t obj;
+    cfg_header_type0_t *obj;
+    cfg_header_type1_t *obj_type1;
+    uint8_t pci_cfg[sizeof(cfg_header_type0_t)];
 
-    PRINT_PCI_DEV_SYMBOL();
+    pcie_read_config_header(bdf, (void *)&pci_cfg[0]);
+    obj = (cfg_header_type0_t *)&pci_cfg[0];
 
-    pcie_read_config_header(bdf, &obj);
-    PRINT_LINE("Vendor-ID: %#x", obj.vendor_id);
-    PRINT_LINE("Device-ID: %#x", obj.device_id);
-    PRINT_LINE("BA0: %#x", obj.base_addr[0]);
-    PRINT_LINE("BA0: %#x", obj.base_addr[1]);
-    PRINT_LINE("BA0: %#x", obj.base_addr[2]);
-    PRINT_LINE("BA0: %#x", obj.base_addr[3]);
-    PRINT_LINE("BA0: %#x", obj.base_addr[4]);
-    PRINT_LINE("BA0: %#x", obj.base_addr[5]);
-    PRINT_LINE("Class-Code: %#x", obj.class_code);
-    PRINT_LINE("Header-Type: %#x", obj.header_type);
-    PRINT_LINE("Interrupt-Line: %#x", obj.interrupt_line);
-    PRINT_LINE("Interrupt-Pin: %#x", obj.interrupt_pin);
+    if (obj->vendor_id == 0xffff) return;
+    if (obj->class_code.base_class == 6) {
+        obj_type1 = (cfg_header_type0_t *)&pci_cfg[0];
+        PRINT_PCI_BRIDGE_SYMBOL();
+        PRINT_LINE("BDF:  %d:%d:%d", (bdf >> 8), ((bdf >> 3) & 0x01F), (bdf & 0x07));
+        PRINT_LINE("Class-Code: %s", classcode_to_text(&obj_type1->class_code));
+        PRINT_LINE("Vendor-ID: %#x", obj_type1->vendor_id);
+        PRINT_LINE("Device-ID: %#x", obj_type1->device_id);
+        PRINT_LINE("Header-Type: %#x", obj_type1->header_type);
+        PRINT_LINE("BA0: %#x", obj_type1->base_addr[0]);
+        PRINT_LINE("BA1: %#x", obj_type1->base_addr[1]);
+
+        PRINT_LINE("Primary bus num: %#x", obj_type1->primary_bus_num);
+        PRINT_LINE("Secondary bus num: %#x", obj_type1->secondary_bus_num);
+        PRINT_LINE("Subordinate bus num: %#x", obj_type1->subordinate_bus_num);
+
+        PRINT_LINE("Interrupt-Line: %#x", obj_type1->interrupt_line);
+        PRINT_LINE("Interrupt-Pin: %#x", obj_type1->interrupt_pin);
+    } else {
+        PRINT_PCI_DEV_SYMBOL();
+        PRINT_LINE("BDF:  %d:%d:%d", (bdf >> 8), ((bdf >> 3) & 0x01F), (bdf & 0x07));
+        PRINT_LINE("Class-Code: %s", classcode_to_text(&obj->class_code));
+        PRINT_LINE("Vendor-ID: %#x", obj->vendor_id);
+        PRINT_LINE("Device-ID: %#x", obj->device_id);
+        PRINT_LINE("Header-Type: %#x", obj->header_type);
+        PRINT_LINE("BA0: %#x", obj->base_addr[0]);
+        PRINT_LINE("BA1: %#x", obj->base_addr[1]);
+        PRINT_LINE("BA2: %#x", obj->base_addr[2]);
+        PRINT_LINE("BA3: %#x", obj->base_addr[3]);
+        PRINT_LINE("BA4: %#x", obj->base_addr[4]);
+        PRINT_LINE("BA5: %#x", obj->base_addr[5]);
+        PRINT_LINE("SubSytem Vendor-Id: %#x", obj->subsystem_vendor_id);
+        PRINT_LINE("SubSytem Id: %#x", obj->subsystem_id);
+        PRINT_LINE("Interrupt-Line: %#x", obj->interrupt_line);
+        PRINT_LINE("Interrupt-Pin: %#x", obj->interrupt_pin);
+    }
     return;
 }
 
 
 void pcie_device_enum()
 {
-    dump_pcie_dev_info(BDF(0, 9, 0));
+
+    for (uint16_t bus = 0; bus < 256; bus++) {
+        for (uint8_t device = 0; device < 32; device++) {
+            for (uint8_t function = 0; function < 8; function++) {
+                dump_pcie_dev_info(BDF(bus, device, function));
+            }
+        }
+    }
+
+//    dump_pcie_dev_info(BDF(0, 1, 3));
     return;
 }
 
 
+char* classcode_to_text(class_code_t *classcode)
+{
+    const char* p_text;
 
+    switch (classcode->base_class) {
+      case 0x00:
+          #define  CLASS0_TEXT  "Device "
+          p_text = CLASS0_TEXT;
+          break;
+      case 0x01:
+          #define  CLASS1_TEXT   "Mass storage controller "
+          p_text = CLASS1_TEXT;
+          break;
+      case 0x02:
+          #define  CLASS2_TEXT   "Network controller "
+          p_text = CLASS2_TEXT;
+          break;
+      case 0x03:
+          #define  CLASS3_TEXT   "Display controller "
+          p_text = CLASS3_TEXT;
+          break;
+      case 0x04:
+          #define  CLASS4_TEXT   "Multimedia device "
+          p_text = CLASS4_TEXT;
+          break;
+      case 0x05:
+          #define  CLASS5_TEXT   "Memory controller "
+          p_text = CLASS5_TEXT;
+          break;
+      case 0x06:
+          #define  CLASS6_TEXT   "Bridge device "
+          p_text = CLASS6_TEXT;
+          break;
+      case 0x07:
+          #define  CLASS7_TEXT   "Simple communication controllers "
+          p_text = CLASS7_TEXT;
+          break;
+      case 0x08:
+          #define  CLASS8_TEXT   "Base system peripherals "
+          p_text = CLASS8_TEXT;
+          break;
+      case 0x09:
+          #define  CLASS9_TEXT   "Input devices "
+          p_text = CLASS9_TEXT;
+          break;
+      case 0x0A:
+          #define  CLASS10_TEXT   "Docking stations "
+          p_text = CLASS10_TEXT;
+          break;
+      case 0x0B:
+          #define  CLASS11_TEXT   "Processors "
+          p_text = CLASS11_TEXT;
+          break;
+      case 0x0C:
+          #define  CLASS12_TEXT   "Serial bus controllers "
+          p_text = CLASS12_TEXT;
+          break;
+      case 0x0D:
+          #define  CLASS13_TEXT   "Wireless controller "
+          p_text = CLASS13_TEXT;
+          break;
+      case 0x0E:
+          #define  CLASS14_TEXT   "Intelligent I/O controllers "
+          p_text = CLASS14_TEXT;
+          break;
+      case 0x0F:
+          #define  CLASS15_TEXT   "Satellite communication controllers "
+          p_text = CLASS15_TEXT;
+          break;
+      case 0x10:
+          #define  CLASS16_TEXT   "Encryption/Decryption controllers "
+          p_text = CLASS16_TEXT;
+          break;
+      case 0x11:
+          #define  CLASS17_TEXT   "Data acquisition and signal processing controllers "
+          p_text = CLASS17_TEXT;
+          break;
+      case 0x12:
+          #define  CLASS18_TEXT   "Processing accelerators "
+          p_text = CLASS18_TEXT;
+          break;
+      case 0x13:
+          #define  CLASS19_TEXT   "Non-Essential Instrumentation "
+          p_text = CLASS19_TEXT;
+          break;
+      default:
+          break;
+    }
+
+    return p_text;
+}
 
 
 
--- x64_cpu_drivers/x64_pcie.h
+++ x64_cpu_drivers/x64_pcie.h
@@ -25,12 +25,18 @@
 #pragma pack(1)
 
 typedef struct {
+    uint8_t Programming_Interface;
+    uint8_t sub_class;
+    uint8_t base_class;
+} class_code_t;
+
+typedef struct {
     uint16_t vendor_id;
     uint16_t device_id;
     uint16_t command;
     uint16_t status;
-    uint16_t revision_id;
-    uint16_t class_code;
+    uint8_t  revision_id;
+    class_code_t class_code;
     uint8_t  cache_line_size;
     uint8_t  latency_timer;
     uint8_t  header_type;
@@ -49,9 +55,41 @@ typedef struct {
 } cfg_header_type0_t;
 
 
-struct  config_header_type1{
+typedef struct {
+    uint16_t vendor_id;
+    uint16_t device_id;
+    uint16_t command;
+    uint16_t status;
+    uint8_t  revision_id;
+    class_code_t class_code;
+    uint8_t  cache_line_size;
+    uint8_t  latency_timer;
+    uint8_t  header_type;
+    uint8_t  bist;
+    uint32_t base_addr[2];
+    uint8_t  primary_bus_num;
+    uint8_t  secondary_bus_num;
+    uint8_t  subordinate_bus_num;
+    uint8_t  secondary_latency_timer;
+    uint8_t  io_base;
+    uint8_t  io_limit;
+    uint16_t secondary_status;
+    uint16_t memory_base;
+    uint16_t memory_limit;
+    uint16_t prefetchable_memory_base;
+    uint16_t prefetchable_memory_limit;
+    uint32_t prefetchable_base_upper32;
+    uint32_t prefetchable_limmit_upper32;
+    uint16_t io_base_upper16;
+    uint16_t io_limit_upper16;
+    uint8_t  capability_pointer;
+    uint8_t  reserved[3];
+    uint32_t expansion_rom_base_addr;
+    uint8_t  interrupt_line;
+    uint8_t  interrupt_pin;
+    uint16_t bridge_control;
+} cfg_header_type1_t;
 
-};
 
 
 struct pci_compatible_config_space_t{
@@ -59,8 +97,15 @@ struct pci_compatible_config_space_t{
 };
 
 
-#pragma pack(pop)
 
+
+typedef struct {
+  uint8_t dev_type;
+
+}pci_bus;
+
+
+#pragma pack(pop)
 
 
 /**
